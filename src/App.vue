c74ffc057c312eeb108df2d535651a78b08756c7
Gwapa Ako Kunoh <nylael.nilue18181919@gmail.com>
2019-10-15 20:34:56 +0800

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/solidarity.png">
+    <HelloWorld msg="We can do this TOGETHER <3"/>
   </div>
 </template>
 
